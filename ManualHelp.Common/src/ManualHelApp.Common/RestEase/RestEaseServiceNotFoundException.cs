3153f379c059750838a6b8c4f63352281da7cad8
Oliynuk-Yura <onyfrak1999@gmail.com>
2019-09-03 19:46:02 +0200

    add consul, fabio and restEase

---
+++ ManualHelp.Common/src/ManualHelApp.Common/RestEase/RestEaseServiceNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManualHelp.Common.RestEase
+{
+    public class RestEaseServiceNotFoundException : Exception
+    {
+        public string ServiceName { get; set; }
+
+        public RestEaseServiceNotFoundException(string serviceName) : this(string.Empty, serviceName)
+        {
+        }
+
+        public RestEaseServiceNotFoundException(string message, string serviceName) : base(message)
+        {
+            ServiceName = serviceName;
+        }
+    }
+
+}
